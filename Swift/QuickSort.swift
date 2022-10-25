e7699757b1f8471b583d1af08457a2de0af6eed3
Misaal D'souza <52240946+misaaldsouza@users.noreply.github.com>
2022-10-25 11:14:11 +0530

    Create QuickSort.swift

---
+++ Swift/QuickSort.swift
@@ -0,0 +1,10 @@
+func quicksort<T: Comparable>(_ a: [T]) -> [T] {
+  guard a.count > 1 else { return a }
+
+  let pivot = a[a.count/2]
+  let less = a.filter { $0 < pivot }
+  let equal = a.filter { $0 == pivot }
+  let greater = a.filter { $0 > pivot }
+
+  return quicksort(less) + equal + quicksort(greater)
+}
